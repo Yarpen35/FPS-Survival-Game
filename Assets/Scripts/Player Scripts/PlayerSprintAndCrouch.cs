7a75470f057af2ea41d32d2531b2d0e00c6c4c38
Yarpen35 <71726390+Yarpen35@users.noreply.github.com>
2021-09-13 21:28:13 +0200

    V0.3

--- Assets/Scripts/Player Scripts/PlayerSprintAndCrouch.cs
+++ Assets/Scripts/Player Scripts/PlayerSprintAndCrouch.cs
@@ -16,12 +16,29 @@ public class PlayerSprintAndCrouch : MonoBehaviour
 
     private bool is_Crouching;
 
+    private PlayerFootsteps player_Footsteps;
+
+    private float sprint_Volume = 1f;
+    private float crouch_Volume = 0.1f;
+    private float walk_Volume_Min = 0.2f, walk_Volume_Max = 0.6f;
+
+    private float walk_Step_Distance = 0.4f;
+
+    private float sprint_Step_Distance = 0.25f;
+    private float crouch_Step_Distance = 0.5f;
+
     void Awake() {
         playerMovement = GetComponent<PlayerMovement>();
         look_Root = transform.GetChild(0);
         
+        player_Footsteps = GetComponentInChildren<PlayerFootsteps>();
     }
 
+    void Start() {
+        player_Footsteps.volume_Min = walk_Volume_Min;
+        player_Footsteps.volume_Max = walk_Volume_Max;
+        player_Footsteps.step_Distance = walk_Step_Distance;
+    }
     
     void Update() {
         Sprint();
@@ -31,10 +48,18 @@ public class PlayerSprintAndCrouch : MonoBehaviour
     void Sprint() {
         if(Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching) {
             playerMovement.speed = sprint_Speed;
+
+            player_Footsteps.step_Distance = sprint_Step_Distance;
+            player_Footsteps.volume_Min = sprint_Volume;
+            player_Footsteps.volume_Max = sprint_Volume;
         }
 
         if(Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching) {
             playerMovement.speed = move_Speed;
+
+            player_Footsteps.volume_Min = walk_Volume_Min;
+            player_Footsteps.volume_Max = walk_Volume_Max;
+            player_Footsteps.step_Distance = walk_Step_Distance;
         }
     }
 
@@ -43,9 +68,18 @@ public class PlayerSprintAndCrouch : MonoBehaviour
             if(is_Crouching) {
                 look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
                 playerMovement.speed = move_Speed;
+
+                player_Footsteps.step_Distance = sprint_Step_Distance;
+                player_Footsteps.volume_Min = sprint_Volume;
+                player_Footsteps.volume_Max = sprint_Volume;
+
             } else {
                 look_Root.localPosition = new Vector3(0f, crouch_Height, 0f);
                 playerMovement.speed = crouch_Speed;
+
+                player_Footsteps.step_Distance = crouch_Step_Distance;
+                player_Footsteps.volume_Min = crouch_Volume;
+                player_Footsteps.volume_Max = crouch_Volume;
 
                 is_Crouching = true;
             }
